04c597c2d1d099770efa21d3d444e8ee0522afaa
Lennard Grimm <hi@grimmle.com>
2022-09-29 20:37:35 +0200

    remove outdated button handler function

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -28,37 +28,13 @@ public class ButtonHandler : MonoBehaviour {
         await SpatialAnchorsSetup.SetupCloudSessionAsync();
         var anchorId = await SpatialAnchorsSetup.SaveCurrentObjectAnchorToCloudAsync();
 
-        //Serialize the SketchWorld to a XML file
-        //SavePath = System.IO.Path.Combine(Application.persistentDataPath, "Sketch-" + System.DateTime.UtcNow.Year + "-" + System.DateTime.UtcNow.Month + "-" + System.DateTime.UtcNow.Day + "-" + System.DateTime.UtcNow.Minute + "-" + System.DateTime.UtcNow.Second + ".xml");
+        //serialize the SketchWorld to a XML file
         savePath = System.IO.Path.Combine(Application.persistentDataPath, anchorId + ".xml");
         SketchWorld.SaveSketchWorld(savePath);
 
-        //Export the SketchWorld as an OBJ file
+        //export the SketchWorld as an OBJ file
         // SketchWorld.ExportSketchWorldToDefaultPath();
     }
-
-    /*public async void Load(string anchorId) {
-        //Create another SketchWorld and load the serialized SketchWorld
-        // DeserializedSketchWorld = Instantiate(Defaults.SketchWorldPrefab).GetComponent<SketchWorld>();
-        DirectoryInfo d = new DirectoryInfo(Application.persistentDataPath);
-        string fileName = "";
-        foreach (var file in d.GetFiles("*.xml")) {
-            Debug.Log("file.Name");
-            fileName = Path.GetFileNameWithoutExtension(file.FullName);
-            Debug.Log(fileName);
-            Debug.Log(file.DirectoryName);
-            SavePath = System.IO.Path.Combine(Application.persistentDataPath, file.Name);
-        }
-        string id = fileName;
-        CloudSpatialAnchor anchor = await asaScript.getAnchorWithId(id);
-        Debug.Log($"returned anchor {anchor.Identifier}");
-        foreach (var kvp in anchor.AppProperties) {
-            Debug.Log($"Key: {kvp.Key}, Value: {kvp.Value}");
-        }
-
-        asaScript.SetAnchorIdsToLocate(new string[] { id });
-        SketchWorld.LoadSketchWorld(SavePath);
-    }*/
 
     public async void LookForNearbySketches() {
         // start asa cloud session and start looking for nearby anchors
